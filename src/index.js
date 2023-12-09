de77102e8978a66a233239200545d1199a35eac3
xauravww <sauravmaheshwari8@gmail.com>
2023-12-09 19:38:35 +0530

    iterating lists and key props

--- src/index.js
+++ src/index.js
@@ -5,38 +5,40 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 
 //prop
-
-const firstBook = {
-  title: "The Power of Your Subconscious Mind",
-  author: "Joseph Murphy",
-  img: "https://cdn.kobo.com/book-images/6f3cf06c-4811-42d4-bd63-564c8264bc2d/1200/1200/False/the-power-of-your-subconscious-mind-57.jpg"
-}
-
-const secondBook = {
-  title: "Think And Grow Rich [Original Edition",
-  author: "Napoleon Hill",
-  img: "https://images-eu.ssl-images-amazon.com/images/I/61B84NiWabL._AC_UL900_SR900,600_.jpg"
-}
+const books = [
+  {
+    id: 1,
+    title: "The Power of Your Subconscious Mind",
+    author: "Joseph Murphy",
+    img: "https://cdn.kobo.com/book-images/6f3cf06c-4811-42d4-bd63-564c8264bc2d/1200/1200/False/the-power-of-your-subconscious-mind-57.jpg"
+  },
+  {
+    id: 2,
+    title: "Think And Grow Rich [Original Edition",
+    author: "Napoleon Hill",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/61B84NiWabL._AC_UL900_SR900,600_.jpg"
+  },
+  {
+    id: 3,
+    title: "THE 48 LAWS OF POWER",
+    author: "Robert Greene",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/41cF79iFTHL._AC_UL900_SR900,600_.jpg"
+  },
+  {
+    id: 4,
+    title: "Do It Today",
+    author: "Darius Foroux",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/61jCkOVf1oL._AC_UL900_SR900,600_.jpg"
+  }
+]
 
 function BookList() {
   return (
     <>
       <section className="bookList">
-        <Book
-          title={firstBook.title}
-          author={firstBook.author}
-          img={firstBook.img}
-        >
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem iste
-            minus ipsa hic nam error cum, beatae quod eos delectus.
-          </p>
-        </Book>
-        <Book
-          title={secondBook.title}
-          author={secondBook.author}
-          img={secondBook.img}
-        />
+        {books.map((book) => {
+          return <Book key={book.id} book={book}></Book>
+        })}
       </section>
     </>
   )
@@ -45,13 +47,14 @@ function BookList() {
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<BookList />)
 
-const Book = ({ img, title, author, children }) => {
+//component
+const Book = (props) => {
+  const { img, title, author } = props.book
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
