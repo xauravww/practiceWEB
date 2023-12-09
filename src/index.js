ecc56f2848186696fc54cd02565d932ff27bfd8d
xauravww <sauravmaheshwari8@gmail.com>
2023-12-09 17:53:26 +0530

    learned props and using js expressions inside jsx

--- src/index.js
+++ src/index.js
@@ -3,18 +3,35 @@ import ReactDOM from "react-dom/client"
 
 // import App from "./App"
 import "./index.css"
+
+//prop
+
+const firstBook = {
+  title: "The Power of Your Subconscious Mind",
+  author: "Joseph Murphy",
+  img: "https://cdn.kobo.com/book-images/6f3cf06c-4811-42d4-bd63-564c8264bc2d/1200/1200/False/the-power-of-your-subconscious-mind-57.jpg"
+}
+
+const secondBook = {
+  title: "Think And Grow Rich [Original Edition",
+  author: "Napoleon Hill",
+  img: "https://images-eu.ssl-images-amazon.com/images/I/61B84NiWabL._AC_UL900_SR900,600_.jpg"
+}
+
 function BookList() {
   return (
     <>
-      <AboutProject />
       <section className="bookList">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        <Book
+          title={firstBook.title}
+          author={firstBook.author}
+          img={firstBook.img}
+        />
+        <Book
+          title={secondBook.title}
+          author={secondBook.author}
+          img={secondBook.img}
+        />
       </section>
     </>
   )
@@ -23,38 +40,12 @@ function BookList() {
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<BookList />)
 
-//explicitly creating elements
-const AboutProject = () => {
-  return React.createElement("h1", {}, "BookList Project ")
-}
-
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
-}
-
-const Image = () => {
-  return (
-    <img
-      src="https://cdn.kobo.com/book-images/6f3cf06c-4811-42d4-bd63-564c8264bc2d/1200/1200/False/the-power-of-your-subconscious-mind-57.jpg"
-      alt=""
-    />
-  )
-}
-
-const Title = () => {
-  return (
-    <h1 style={{ fontSize: "1.5rem", color: "#fe4345" }}>
-      The Power of Your Subconscious Mind
-    </h1>
-  )
-}
-
-const Author = () => {
-  return <div>Joseph Murphy</div>
 }
